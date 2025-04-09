0a16f6a987128c7e20a474255a877442969496f2
mihir83in <mihir83in@yahoo.com>
2025-04-09 13:19:32 +0000

    feat: added api route

--- pages/ssr.tsx
+++ pages/ssr.tsx
@@ -1,23 +1,27 @@
 // pages/ssr.tsx
 
 type SSRPageProps = {
-  timestamp: string
+  timestamp: string,
+  apiMessage: string
 }
 
 export async function getServerSideProps() {
-  const timestamp = new Date().toISOString()
+  const res = await fetch('https://react-job-search-swart.vercel.app/api/hello')
+  const data = await res.json()
+
   return {
     props: {
-      timestamp
-    }
+      apiMessage: data.message,
+      timestamp: new Date().toISOString(),
+    },
   }
 }
 
-export default function SSRPage({ timestamp }: SSRPageProps) {
+export default function SSRPage({ apiMessage, timestamp }: SSRPageProps) {
   return (
-    <div className="p-6 text-xl font-mono">
-      <p>🚀 This page was rendered at: </p>
-      <p className="text-blue-600 font-bold">{timestamp}</p>
+    <div className="p-6">
+      <p className="text-xl">⏰ Server time: <span className="font-bold">{timestamp}</span></p>
+      <p className="text-lg mt-4">💬 API says: <span className="text-blue-600">{apiMessage}</span></p>
     </div>
   )
 }
